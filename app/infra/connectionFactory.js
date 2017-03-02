8705e6e584df8785e880da51eda67bc7d6f735f5
Pablo <pablomf84@gmail.com>
2017-03-01 22:38:39 -0300

    Ajuste das informações de conexão

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,13 +1,17 @@
 var pgp = require('pg-promise')({});
-var _connectionPath_Development = 'postgres://postgres:123@localhost:5432/casadocodigo_nodejs22';
+var _connectionPath_Production = process.env.DATABASE_URL;
+var _connectionPath_Development = 'postgres://postgres:123@localhost:5432/casadocodigo_nodejs';
 var _connectionPath_Test = 'postgres://postgres:123@localhost:5432/casadocodigo_nodejs_test';
 var client;
 
 function createDBConnection() {
-	if(process.env.NODE_ENV == "test")
+	if(process.env.NODE_ENV == "test") {
 		client = pgp(_connectionPath_Test);
-	else
+	} else if(process.env.NODE_ENV == "development") {
 		client = pgp(_connectionPath_Development);
+	} else {
+		client = pgp(_connectionPath_Production);
+	}
 	client.connect();
 	console.log('Conexão realizada com sucesso!');
 }
